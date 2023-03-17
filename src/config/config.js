803f9754fb31f9165cd81f85c5a334eecb8851a5
skymarocchinia <92101997+skymarocchinia@users.noreply.github.com>
2023-03-17 16:23:45 +0100

    feat(code): poc - ball animation pass vertical field

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 // Utilizzo del metodo
-export const field_width = 700; // Larghezza del campo di calcio
+export const field_width = 250; // Larghezza del campo di calcio
 export const field_height = 400; // Altezza del campo di calcio
